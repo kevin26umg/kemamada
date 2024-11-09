ffb11612670fe1e5722d918c8718e22cb3caaef9
Kevin <khernandezm27@miumg.edu.gt>
2024-11-08 22:42:07 -0600

    dark mode

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -83,10 +83,10 @@ document.querySelectorAll('.comment-form').forEach(form => {
         const commentContainer = document.querySelector(`#comments-${postId}`);
             if (commentContainer) {
             const commentElement = document.createElement('div');
-            commentElement.className = 'bg-gray-100 p-4 rounded mt-2';
+            commentElement.className = 'bg-gray-100 p-4 rounded mt-2 text-gray-500 dark:bg-gray-700/60 dark:text-gray-200';
             commentElement.innerHTML = `
                 <p>${comment.content}</p>
-                <p class="text-sm text-gray-500">Comentado por ${comment.author.username}</p>
+                <p class="text-sm font-semibold text-gray-500 dark:text-gray-200">${comment.author.username}</p>
             `;
             commentContainer.appendChild(commentElement);
             
